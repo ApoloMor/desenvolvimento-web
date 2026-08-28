c30d87bc414975bafec3b49f1a961363dcfdc1b5
Apolo mor <apolomsilva@gmail.com>
2026-08-28 15:49:25 -0300

    algumas ai

--- TDEs/opcional3/script.js
+++ TDEs/opcional3/script.js
@@ -1,12 +1,32 @@
 function btnSalvar() {
-  const nome = document.getElementById("nome");
-  const anoLancamento = document.getElementById("ano-lancamento");
-  const desenvolvedora = document.getElementById("desenvolvedora");
-  const distribuidora = document.getElementById("distribuidora");
+  const nome = document.getElementById("nome").value;
+  const anoLancamento = document.getElementById("ano-lancamento").value;
+  const desenvolvedora = document.getElementById("desenvolvedora").value;
+  const distribuidora = document.getElementById("distribuidora").value;
   const jogo = {
     nome: nome,
     anoLancamento: anoLancamento,
     desenvolvedora: desenvolvedora,
     distribuidora: distribuidora,
   };
+
+  let jogos = JSON.parse(localStorage.getItem("jogos")) || [];
+
+  jogos.push(jogo);
+
+  localStorage.setItem("jogos", JSON.stringify(jogos));
+
+  listarJogos();
+}
+
+function listarJogos() {
+  const lista = document.getElementById("listaJogos");
+
+  let jogos = JSON.parse(localStorage.getItem("jogos")) || [];
+
+  lista.innerHTML = "";
+
+  jogos.forEach((jogo) => {
+    lista.innerHTML += `<p>${jogo.nome}</p>`;
+  });
 }
