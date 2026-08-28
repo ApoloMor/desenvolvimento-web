9fe00eb20f75d9297231dfcfc906aa76be3847ee
Apolo <apolomsilva@gmail.com>
2026-08-28 10:59:16 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    SE TAVA ESQUECENDO DE MIM????????

---
+++ TDEs/opcional3/script.js
@@ -0,0 +1,12 @@
+function btnSalvar() {
+  const nome = document.getElementById("nome");
+  const anoLancamento = document.getElementById("ano-lancamento");
+  const desenvolvedora = document.getElementById("desenvolvedora");
+  const distribuidora = document.getElementById("distribuidora");
+  const jogo = {
+    nome: nome,
+    anoLancamento: anoLancamento,
+    desenvolvedora: desenvolvedora,
+    distribuidora: distribuidora,
+  };
+}
